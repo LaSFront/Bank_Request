5f88f0b15a7268e83fd2a5667fe0e5b4252ced60
“Larisa” <lsergeevna1978@mail.ru>
2023-07-20 08:56:54 +0400

    validation of fields

--- src/test/java/ru/netology/web/BankRequestTest.java
+++ src/test/java/ru/netology/web/BankRequestTest.java
@@ -33,4 +33,71 @@ public class BankRequestTest {
 
         Thread.sleep(5000);
     }
+
+    @Test
+    void shouldCheckNameValidationFieldInvalid() throws InterruptedException {
+        open("http://localhost:9999");
+        $("[name=name]").setValue("Ggggggg");
+        $("button").click();
+        ElementsCollection form = $$(".input__sub");
+        form.get(0).shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $(".input_invalid").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        Thread.sleep(5000);
+    }
+
+    @Test
+    void shouldCheckNameValidationFieldEmpty() throws InterruptedException {
+        open("http://localhost:9999");
+        $("[name=phone]").setValue("+77777777777");
+        $(".checkbox__box").click();
+        $("button").click();
+        ElementsCollection form = $$(".input__sub");
+        form.get(0).shouldHave(text("Поле обязательно для заполнения"));
+        $(".input_invalid").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        Thread.sleep(5000);
+    }
+
+    @Test
+    void shouldCheckTelephoneValidationFieldInvalid() throws InterruptedException {
+        open("http://localhost:9999");
+        $("[name=name]").setValue("Аааааа");
+        $("[name=phone]").setValue("+9999999999");
+        $("button").click();
+        ElementsCollection form = $$(".input__sub");
+        form.get(1).shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $(".input_invalid").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        Thread.sleep(5000);
+    }
+
+    @Test
+    void shouldCheckTelephoneValidationFieldEmpty() throws InterruptedException {
+        open("http://localhost:9999");
+        $("[name=name]").setValue("Аааааа");
+        $(".checkbox__box").click();
+        $("button").click();
+        ElementsCollection form = $$(".input__sub");
+        form.get(1).shouldHave(text("Поле обязательно для заполнения"));
+        $(".input_invalid").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        Thread.sleep(5000);
+    }
+
+    @Test
+    void shouldCheckCheckBoxValidationFieldEmpty() throws InterruptedException {
+        open("http://localhost:9999");
+        $("[name=name]").setValue("Аааааа");
+        $("[name=phone]").setValue("+99999999999");
+        $("button").click();
+        $(".input_invalid").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        Thread.sleep(5000);
+    }
+
+    @Test
+    void shouldNotSendRequestWhenAllFieldEmpty() throws InterruptedException {
+        open("http://localhost:9999");
+        $("button").click();
+        ElementsCollection form = $$(".input__sub");
+        form.get(0).shouldHave(text("Поле обязательно для заполнения"));
+        $(".input_invalid").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+        Thread.sleep(5000);
+    }
 }
